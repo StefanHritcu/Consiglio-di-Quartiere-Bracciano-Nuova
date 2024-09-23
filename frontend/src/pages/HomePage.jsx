01bbb2b87712311f4f0e2b03cc82d57d5d8983d4
StefanHritcu <shritcu@proton.me>
2024-09-23 21:22:15 +0200

    add: homepage text example

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,85 @@
-import InfoDescription from "./homePageCOMPONENTS/InfoDescription";
-
 function HomePage() {
   return (
     <>
       <div className="mt-24 sm:mt-38 xl:mt-52 h-auto">
-        <InfoDescription />
+        <h1 className="text-3xl sm:text-4xl xl:text-5xl font-bold text-gray-800 leading-tight mb-6">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+          repudiandae cupiditate in excepturi saepe facere itaque magnam nulla
+          totam dolor?
+        </h1>
+        <p className="text-lg text-gray-600 leading-relaxed">
+          Vel voluptas rerum vero hic enim odio ipsa blanditiis dolorem, illum
+          id veniam, tempore esse! Reprehenderit neque ratione autem iste esse
+          officia! Nulla possimus ut hic minus officiis corporis praesentium
+          laborum dolorum, nesciunt cum libero iure mollitia veniam similique
+          facilis reiciendis magni aperiam. Veniam beatae libero amet minima
+          architecto illum quos vel sapiente exercitationem nisi labore itaque
+          maiores dolore sit voluptate laboriosam facilis esse ut, perspiciatis
+          sint! Repellendus fuga quibusdam quisquam rem quos consectetur
+          explicabo quas quod dicta? Corrupti est, at voluptas, quis cupiditate
+          eum ipsam minus molestiae optio maiores delectus nostrum reprehenderit
+          hic tempora. Itaque tempore laborum molestias eos placeat?
+        </p>
+      </div>
+      <div className="mt-24 sm:mt-38 xl:mt-52 h-auto">
+        <h1 className="text-3xl sm:text-4xl xl:text-5xl font-bold text-gray-800 leading-tight mb-6">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+          repudiandae cupiditate in excepturi saepe facere itaque magnam nulla
+          totam dolor?
+        </h1>
+        <p className="text-lg text-gray-600 leading-relaxed">
+          Vel voluptas rerum vero hic enim odio ipsa blanditiis dolorem, illum
+          id veniam, tempore esse! Reprehenderit neque ratione autem iste esse
+          officia! Nulla possimus ut hic minus officiis corporis praesentium
+          laborum dolorum, nesciunt cum libero iure mollitia veniam similique
+          facilis reiciendis magni aperiam. Veniam beatae libero amet minima
+          architecto illum quos vel sapiente exercitationem nisi labore itaque
+          maiores dolore sit voluptate laboriosam facilis esse ut, perspiciatis
+          sint! Repellendus fuga quibusdam quisquam rem quos consectetur
+          explicabo quas quod dicta? Corrupti est, at voluptas, quis cupiditate
+          eum ipsam minus molestiae optio maiores delectus nostrum reprehenderit
+          hic tempora. Itaque tempore laborum molestias eos placeat?
+        </p>
+      </div>
+      <div className="mt-24 sm:mt-38 xl:mt-52 h-auto">
+        <h1 className="text-3xl sm:text-4xl xl:text-5xl font-bold text-gray-800 leading-tight mb-6">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+          repudiandae cupiditate in excepturi saepe facere itaque magnam nulla
+          totam dolor?
+        </h1>
+        <p className="text-lg text-gray-600 leading-relaxed">
+          Vel voluptas rerum vero hic enim odio ipsa blanditiis dolorem, illum
+          id veniam, tempore esse! Reprehenderit neque ratione autem iste esse
+          officia! Nulla possimus ut hic minus officiis corporis praesentium
+          laborum dolorum, nesciunt cum libero iure mollitia veniam similique
+          facilis reiciendis magni aperiam. Veniam beatae libero amet minima
+          architecto illum quos vel sapiente exercitationem nisi labore itaque
+          maiores dolore sit voluptate laboriosam facilis esse ut, perspiciatis
+          sint! Repellendus fuga quibusdam quisquam rem quos consectetur
+          explicabo quas quod dicta? Corrupti est, at voluptas, quis cupiditate
+          eum ipsam minus molestiae optio maiores delectus nostrum reprehenderit
+          hic tempora. Itaque tempore laborum molestias eos placeat?
+        </p>
+      </div>
+      <div className="mt-24 sm:mt-38 xl:mt-52 h-auto">
+        <h1 className="text-3xl sm:text-4xl xl:text-5xl font-bold text-gray-800 leading-tight mb-6">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+          repudiandae cupiditate in excepturi saepe facere itaque magnam nulla
+          totam dolor?
+        </h1>
+        <p className="text-lg text-gray-600 leading-relaxed">
+          Vel voluptas rerum vero hic enim odio ipsa blanditiis dolorem, illum
+          id veniam, tempore esse! Reprehenderit neque ratione autem iste esse
+          officia! Nulla possimus ut hic minus officiis corporis praesentium
+          laborum dolorum, nesciunt cum libero iure mollitia veniam similique
+          facilis reiciendis magni aperiam. Veniam beatae libero amet minima
+          architecto illum quos vel sapiente exercitationem nisi labore itaque
+          maiores dolore sit voluptate laboriosam facilis esse ut, perspiciatis
+          sint! Repellendus fuga quibusdam quisquam rem quos consectetur
+          explicabo quas quod dicta? Corrupti est, at voluptas, quis cupiditate
+          eum ipsam minus molestiae optio maiores delectus nostrum reprehenderit
+          hic tempora. Itaque tempore laborum molestias eos placeat?
+        </p>
       </div>
     </>
   );
