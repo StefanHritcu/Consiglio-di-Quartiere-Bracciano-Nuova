43bc0907ed27d778477faca336ace4a1e7074320
StefanHritcu <shritcu@proton.me>
2024-09-26 11:18:28 +0200

    refactor: aggiunta di piccole modifiche nella pagina HomePage

--- frontend/src/pages/homePageCOMPONENTS/ShortLastArticle.jsx
+++ frontend/src/pages/homePageCOMPONENTS/ShortLastArticle.jsx
@@ -1,37 +1,59 @@
 import articles from "../../DATA/Articles";
+import { IoMdPerson } from "react-icons/io";
+import { MdAccessTime } from "react-icons/md";
+import { CiCalendarDate } from "react-icons/ci";
+import { Link } from "react-router-dom";
 
 function ShortLastArticle() {
-  const lastArticle = articles[0];
+  {
+    /* PRENDO L ULTIMO OGGETTO DELL'ARRAY DEL JSON ARTICLES.JS */
+  }
+  const lastArticle = articles[articles.length - 1];
 
   return (
-    <div className="max-w-2xl mx-auto p-6 bg-white shadow-md rounded-lg mt-8 xl:w-screen xl:mx-16">
+    <div className="max-w-2xl mx-auto p-10 bg-white shadow-md rounded-lg mt-8 xl:w-screen xl:mx-16">
       <h1 className="text-2xl font-bold text-center text-gray-800">
         Ultima Notizia
       </h1>
-      <div className="mt-4">
+      <Link
+        to="/lastarticle"
+        className="pt-6  hover:scale-105 transition-transform duration-300 block"
+      >
+        {/* IMMAGINE */}
         <img
           src={lastArticle.imageUrl}
           alt={lastArticle.title}
           className="w-full h-60 object-cover rounded-lg"
         />
+        {/* TITOLO */}
         <h2 className="mt-4 text-xl font-semibold text-gray-800">
           {lastArticle.title}
         </h2>
+        {/* DESCIZIONE PICCOLA */}
         <p className="mt-2 text-lg text-gray-600">
           {lastArticle.shortDescription}
         </p>
-        <p className="mt-1 text-sm text-gray-500">
+        {/* AUTORE */}
+        <p className="mt-1 flex items-center text-sm text-gray-500">
+          <IoMdPerson className="mr-1" />
           Autore: {lastArticle.author}
         </p>
-        <p className="mt-1 text-sm text-gray-500">
+        {/* DATA DI PUBBLICAZIONE */}
+        <p className="mt-1 flex items-center text-sm text-gray-500">
+          <CiCalendarDate className="mr-1" />
           Data: {new Date(lastArticle.createdAt).toLocaleDateString()}
         </p>
-        <p className="mt-1 text-sm text-gray-500">
+        {/* TEMPO DI LETTURA */}
+        <p className="mt-1 flex items-center text-sm text-gray-500">
+          <MdAccessTime className="mr-1" />
           Tempo di lettura: {lastArticle.readingTime}
         </p>
+        {/* CONTENUTO DELL'ARTICOLO CON MASSIMO 300 LETTERE */}
         <p className="mt-4 text-md text-gray-700">
-          {lastArticle.fullDescription}
+          {lastArticle.content.slice(0, 300)}
+          {lastArticle.content.length > 300 ? "..." : ""}
         </p>
+        {/* HASTAGS  */}
         <div className="mt-4">
           {lastArticle.hashtags.map((hashtag) => (
             <span key={hashtag} className="inline-block mr-2 text-blue-600">
@@ -39,7 +61,7 @@ function ShortLastArticle() {
             </span>
           ))}
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
