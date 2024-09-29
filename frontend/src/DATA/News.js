aab709ee5fb0edfbdb7523be85194f8101c1ed31
StefanHritcu <shritcu@proton.me>
2024-09-29 16:49:26 +0200

    feat: ArticoliSection added with 3 main categorys event news and artilces with the 4 last post

---
+++ frontend/src/DATA/News.js
@@ -0,0 +1,57 @@
+const news = [
+  {
+    title: "Piccolo allagamento in Via delle Viole",
+    author: "Redazione Locale",
+    description_short: "Allagamento in Via delle Viole a causa della pioggia.",
+    description_long:
+      "Un forte acquazzone ha causato un piccolo allagamento in Via delle Viole, ma non ci sono danni significativi.",
+    content:
+      "Questa mattina, a seguito di un violento acquazzone, si è verificato un piccolo allagamento in Via delle Viole. La situazione è sotto controllo, e i tecnici comunali stanno già intervenendo per liberare le grondaie e ripristinare il normale deflusso delle acque. Non si segnalano danni significativi né interruzioni alla viabilità.",
+    date: "2024-09-29T11:00:00",
+    location: "Via delle Viole, Bracciano",
+    severity: "low",
+    affected_area: "Centro, Bracciano",
+  },
+  {
+    title: "Attenzione: lavori stradali in Via Roma",
+    author: "Ufficio Stampa Comune di Bracciano",
+    description_short: "Lavori di manutenzione in corso su Via Roma.",
+    description_long:
+      "In corso lavori di manutenzione su Via Roma, si consiglia di prestare attenzione.",
+    content:
+      "A partire da oggi, sono iniziati i lavori di manutenzione su Via Roma. Il Comune di Bracciano invita i cittadini a prestare attenzione durante la circolazione, in quanto potrebbero esserci deviazioni e rallentamenti. I lavori sono previsti per concludersi entro la fine della settimana.",
+    date: "2024-09-29T10:30:00",
+    location: "Via Roma, Bracciano",
+    severity: "medium",
+    affected_area: "Centro, Bracciano",
+  },
+  {
+    title: "Controlli di sicurezza in corso nel Parco della Rimembranza",
+    author: "Guardia Forestale",
+    description_short:
+      "Controlli di sicurezza effettuati nel Parco della Rimembranza.",
+    description_long:
+      "La Guardia Forestale sta effettuando controlli di sicurezza nel Parco della Rimembranza per garantire la sicurezza dei visitatori.",
+    content:
+      "Oggi, la Guardia Forestale ha avviato controlli di sicurezza nel Parco della Rimembranza per garantire un ambiente sicuro per i visitatori. Gli agenti stanno ispezionando le aree più frequentate e invitano tutti a rispettare le norme di sicurezza durante le visite. Non si segnalano problemi al momento.",
+    date: "2024-09-29T12:00:00",
+    location: "Parco della Rimembranza, Bracciano",
+    severity: "low",
+    affected_area: "Zona Verde, Bracciano",
+  },
+  {
+    title: "Progetto di riqualificazione della Piazza principale",
+    author: "Comune di Bracciano",
+    description_short:
+      "Iniziato il progetto di riqualificazione della Piazza principale.",
+    description_long:
+      "Un nuovo progetto di riqualificazione per migliorare la Piazza principale è stato avviato dal Comune.",
+    content:
+      "Il Comune di Bracciano ha avviato un progetto di riqualificazione della Piazza principale per migliorarne l'estetica e la funzionalità. I lavori includeranno la sistemazione dei pavimenti, l'aggiunta di nuove piante e la creazione di spazi per eventi pubblici. La prima fase dei lavori è prevista per essere completata entro il prossimo mese.",
+    date: "2024-09-29T13:00:00",
+    location: "Piazza principale, Bracciano",
+    severity: "low",
+    affected_area: "Centro, Bracciano",
+  },
+];
+export default news;
--- frontend/src/components/articoli/ArticoliSection.jsx
+++ frontend/src/components/articoli/ArticoliSection.jsx
@@ -1,15 +1,158 @@
 import { Helmet } from "react-helmet-async";
+import events from "../../DATA/Events";
+import news from "../../DATA/News";
+import articles from "../../DATA/Articles";
+import { IoMdPerson } from "react-icons/io";
+import { MdAccessTime } from "react-icons/md";
+import { CiCalendarDate } from "react-icons/ci";
+import { Link } from "react-router-dom";
+import LastArticleExampleImage from "./../../assets/images/lastArticleExampleImage.jpg";
+import { GrArticle } from "react-icons/gr";
 
 function ArticoliSection() {
+  // Ordina le notizie, gli eventi e gli articoli
+  const sortedNews = news
+    .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+    .slice(0, 4);
+  const sortedEvents = events
+    .sort((a, b) => new Date(a.data) - new Date(b.data))
+    .slice(0, 4);
+  const sortedArticles = articles
+    .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
+    .slice(0, 4);
+
   return (
     <>
       <Helmet>
-        {/* Metadati specifici per la pagina Articoli */}
         <title>Articoli - Bracciano Nuova CqD</title>
         <meta name="description" content="Browse through our articoli." />
       </Helmet>
-      <div>
-        <h1>Sezione degli articoli, eventi e altro</h1>
+      <div className="container mx-auto p-4">
+        <h1 className="text-3xl font-bold mb-6">
+          Sezione degli articoli, eventi e altro
+        </h1>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-8">
+          {/* Sezione Notizie */}
+          <div className="bg-white rounded-lg shadow-md p-4">
+            <h2 className="text-xl font-semibold mb-4">Notizie</h2>
+            {sortedNews.map((item, index) => (
+              <div
+                key={index}
+                className={`bg-gray-100 rounded-lg p-4 mb-4 transition-transform transform hover:scale-105 ${
+                  index === 0 ? "border-4 border-primary" : ""
+                }`}
+              >
+                {index === 0 && (
+                  <span className="absolute top-2 left-2 bg-red-500 text-white text-xs font-bold py-1 px-2 rounded">
+                    Più recente
+                  </span>
+                )}
+                <Link to={`/news/${item.id}`} className="block">
+                  <img
+                    src={item.imageUrl || LastArticleExampleImage} // Assicurati di avere un'immagine per ogni notizia
+                    alt={item.title}
+                    className="w-full h-48 object-cover rounded-lg"
+                  />
+                  <h3 className="font-medium text-lg">{item.title}</h3>
+                  <p className="text-gray-600 mt-2">{item.shortDescription}</p>
+                  <p className="mt-1 flex items-center text-sm text-gray-500">
+                    <IoMdPerson className="mr-1" />
+                    Autore: {item.author}
+                  </p>
+                  <p className="mt-1 flex items-center text-sm text-gray-500">
+                    <CiCalendarDate className="mr-1" />
+                    Data: {new Date(item.createdAt).toLocaleDateString()}
+                  </p>
+                  <p className="mt-1 flex items-center text-sm text-gray-500">
+                    <MdAccessTime className="mr-1" />
+                    Tempo di lettura: {item.readingTime}
+                  </p>
+                  <div className="flex justify-between items-center mt-4">
+                    <Link className="bg-primary text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition duration-300">
+                      Scopri di più
+                    </Link>
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </div>
+
+          {/* Sezione Eventi */}
+          <div className="bg-white rounded-lg shadow-md p-4">
+            <h2 className="text-xl font-semibold mb-4">Eventi</h2>
+            {sortedEvents.map((event, index) => (
+              <div
+                key={event.id}
+                className={`bg-gray-100 rounded-lg p-4 mb-4 transition-transform transform hover:scale-105 ${
+                  index === 0 ? "border-4 border-primary" : ""
+                }`}
+              >
+                {index === 0 && (
+                  <span className="absolute top-2 left-2 bg-red-500 text-white text-xs font-bold py-1 px-2 rounded">
+                    Prossimo evento
+                  </span>
+                )}
+                <Link to={`/events/${event.id}`} className="block">
+                  <h3 className="font-medium text-lg">{event.titolo}</h3>
+                  <p className="text-gray-600 mt-2">{event.descrizioneShort}</p>
+                  <p className="mt-1 flex items-center text-sm text-gray-500">
+                    <CiCalendarDate className="mr-1" />
+                    Data: {new Date(event.data).toLocaleDateString()} -{" "}
+                    {event.orario}
+                  </p>
+                  <div className="flex justify-between items-center mt-4">
+                    <Link className="bg-primary text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition duration-300">
+                      Scopri di più
+                    </Link>
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </div>
+
+          {/* Sezione Articoli */}
+          <div className="bg-white rounded-lg shadow-md p-4">
+            <h2 className="text-xl font-semibold mb-4">Articoli</h2>
+            {sortedArticles.map((article, index) => (
+              <div
+                key={article.id}
+                className={`bg-gray-100 rounded-lg p-4 mb-4 transition-transform transform hover:scale-105 ${
+                  index === 0 ? "border-4 border-primary" : ""
+                }`}
+              >
+                {index === 0 && (
+                  <span className="absolute top-2 left-2 bg-red-500 text-white text-xs font-bold py-1 px-2 rounded">
+                    Articolo più recente
+                  </span>
+                )}
+                <Link to={`/articles/${article.id}`} className="block">
+                  <h3 className="font-medium text-lg">{article.title}</h3>
+                  <p className="text-gray-600 mt-2">
+                    {article.shortDescription}
+                  </p>
+                  <p className="mt-1 flex items-center text-sm text-gray-500">
+                    <IoMdPerson className="mr-1" />
+                    Autore: {article.author}
+                  </p>
+                  <p className="mt-1 flex items-center text-sm text-gray-500">
+                    <CiCalendarDate className="mr-1" />
+                    Data: {new Date(article.createdAt).toLocaleDateString()}
+                  </p>
+                  <p className="mt-1 flex items-center text-sm text-gray-500">
+                    <MdAccessTime className="mr-1" />
+                    Tempo di lettura: {article.readingTime}
+                  </p>
+                  <div className="flex justify-between items-center mt-4">
+                    <Link className="bg-primary text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition duration-300">
+                      Scopri di più
+                    </Link>
+                  </div>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </>
   );
